82bedcf6dae102062cce7871fa7cd4a4d5d1ae49
Shree-G <shree.hridai@gmail.com>
2022-02-04 12:35:33 -0800

    class + tester

---
+++ squareTester.java
@@ -0,0 +1,11 @@
+//imports necessary JUnit tester addons
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class squareTester {
+    
+    @Test
+    public void squaring(){
+        assertEquals(4, square.getTheSquare(2));
+    }
+}
